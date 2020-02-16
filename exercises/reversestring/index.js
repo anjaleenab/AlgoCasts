f5df1369d2626c8892cc73b2abbe5b4bf6a63bf4
anjaleena <anjaleenab@github.com>
2020-02-15 16:55:07 -0800

    completed the reverse string function

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  var reverseStr='';
+  for(var length= str.length-1; length>=0; length--) {
+    reverseStr+= str[length];
+  }
+  return reverseStr;
+}
 
 module.exports = reverse;

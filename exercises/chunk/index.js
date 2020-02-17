e8fbd8e28bb9993e2c99d46b4b1f13753266ee72
anjaleena <anjaleenab@github.com>
2020-02-16 21:26:28 -0800

    completed the array chunk problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   var chunks=[];
+//   var miniChunk = [];
+//   for (var i=0; i<array.length;i++) {
+//     miniChunk.push(array[i]);
+//     if(miniChunk.length === size || i === array.length-1) {
+//       chunks.push(miniChunk);
+//       miniChunk = [];
+//     }
+//   }
+//   return chunks;
+// }
+
+//using slice method
+
+function chunk(array, size) {
+  var index=0;
+  var chunks=[];
+  while(index < array.length) {
+  chunks.push(array.slice(index, index+size));
+  index+=size;
+  }
+  return chunks;
+}
 
 module.exports = chunk;

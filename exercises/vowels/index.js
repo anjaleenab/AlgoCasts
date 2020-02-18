84f78636ffd3ea8500f938be2b2cdc304a954439
anjaleena <anjaleenab@github.com>
2020-02-17 20:05:27 -0800

    completed the vowels problem

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   var vowelCount=0;
+//   var checker='aeiouAEIOU';
+//   for(var char of str) {
+//     if(checker.includes(char)) {
+//       vowelCount++;
+//     }
+//   }
+//   return vowelCount;
+// }
+
+//another solution
+function vowels(str) {
+var matches= str.match(/[aeiou]/gi);   //g makes sure that you do not stop when you find a match,
+                                        //i means case insensitive
+return matches ? matches.length : 0;
+}
+
+
+
 
 module.exports = vowels;

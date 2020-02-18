9051c419184c84845ea4ae537943586540fb134a
anjaleena <anjaleenab@github.com>
2020-02-17 17:27:43 -0800

    completed the capitalize function

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,25 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  var words= [];
+  for(var word of str.split(' ')) {
+   words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(' ');
+}
 
+//a second solution
+
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+//   for(let letter=0; letter<str.length;letter++) {
+//     if(str[letter-1] === ' ') {
+//       result+=str[i].toUpperCase();
+//     } else {
+//       result+=str[i];
+//     }
+//   }
+//   return result;
+// }
 module.exports = capitalize;
